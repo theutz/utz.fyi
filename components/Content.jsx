32721e15e4dea7c8808cb13b5c89aa5a65b41586
Michael Utz <michael@theutz.com>
2018-11-24 16:09:13 +0200

    style: color of content

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
-import { space } from '../theme'
+import { space, colors } from '../theme'
 
 const Content = styled.div`
   max-width: 960px;
   margin: 0 auto;
   margin-top: ${space(5)};
   padding: 0 ${space(2)};
+  color: ${colors.foreground};
 `
 
 export default Content
