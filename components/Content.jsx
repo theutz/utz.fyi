d7e19d4fecbc53a3090c3b454163df5b0339f57b
Michael Utz <michael@theutz.com>
2018-11-24 16:02:19 +0200

    style: content wrapper

---
+++ components/Content.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { space } from '../theme'
+
+const Content = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  margin-top: ${space(5)};
+  padding: 0 ${space(2)};
+`
+
+export default Content
--- pages/background.jsx
+++ pages/background.jsx
@@ -1,6 +1,7 @@
 import Page from '../layouts/main'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Content from '../components/Content'
 
 const Background = () => (
   <Page>
@@ -8,8 +9,28 @@ const Background = () => (
       return (
         <>
           <Header />
-          <h2>Background</h2>
-          <Footer />
+          <Content>
+            <h2>Background</h2>
+            <p>
+              I've been working on the web since before CSS3 was a thing—mostly
+              as a hobby or for local non-profits.
+            </p>
+            <p>
+              In 2007, I graduated from Northwest University with a Bachelor of
+              Arts in Music focusing on music technology. My passion for
+              technology and creativity lead me to learn more about the web.
+            </p>
+            <p>
+              Since then, I've had a chance to work with some wonderful teams to
+              create marketing sites, mostly in WordPress. I caught the
+              JavaScript bug in 2015, and I can't get enough of it!
+            </p>
+            <p>
+              My wife and I moved to Helsinki in February 2018, and we love it
+              here!
+            </p>
+            <Footer />
+          </Content>
         </>
       )
     }}
