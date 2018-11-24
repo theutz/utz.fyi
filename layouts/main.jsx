60e6c24964984cb5860c9bfc9f7f24e3d69c7166
Michael Utz <michael@theutz.com>
2018-11-24 14:57:51 +0200

    refactor: extract <head /> data to separate tag

--- layouts/main.jsx
+++ layouts/main.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components'
 import Head from 'next/head'
 import Media from 'react-media'
 
+import Meta from '../components/Meta'
 import GlobalStyle from '../components/GlobalStyle'
 import { schema } from '../theme'
 import LocalStorage from '../helpers/LocalStorage'
@@ -63,48 +64,8 @@ class Page extends Component {
               >
                 <>
                   <GlobalStyle />
+                  <Meta />
                   <Head>
-                    <link
-                      rel="apple-touch-icon"
-                      sizes="180x180"
-                      href="/static/apple-touch-icon.png"
-                    />
-                    <link
-                      rel="icon"
-                      type="image/png"
-                      sizes="32x32"
-                      href="/static/favicon-32x32.png"
-                    />
-                    <link
-                      rel="icon"
-                      type="image/png"
-                      sizes="16x16"
-                      href="/static/favicon-16x16.png"
-                    />
-                    <link rel="manifest" href="/static/site.webmanifest" />
-                    <link
-                      rel="mask-icon"
-                      href="/static/safari-pinned-tab.svg"
-                      color="#101935"
-                    />
-                    <link rel="shortcut icon" href="/static/favicon.ico" />
-                    <meta name="apple-mobile-web-app-title" content="utz.fyi" />
-                    <meta name="application-name" content="utz.fyi" />
-                    <meta name="msapplication-TileColor" content="#2b5797" />
-                    <meta
-                      name="msapplication-config"
-                      content="/static/browserconfig.xml"
-                    />
-                    <meta name="theme-color" content="#ffffff" />
-                    <meta
-                      name="viewport"
-                      content="initial-scale=1.0, width=device-width"
-                      key="viewport"
-                    />
-                    <link
-                      href="https://fonts.googleapis.com/css?family=Rubik:400,700"
-                      rel="stylesheet"
-                    />
                     <title>Michael Utz, FYI</title>
                   </Head>
                   {this.props.children}
